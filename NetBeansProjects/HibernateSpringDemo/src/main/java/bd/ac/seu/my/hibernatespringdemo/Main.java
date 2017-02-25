9d910086de8d04442369951eb4875c4773472eca
shithilboss <shithilboss@gmail.com>
2017-02-25 08:27:32 +0600

    1st commit but database lol connected............ yeeeeee

--- NetBeansProjects/HibernateSpringDemo/src/main/java/bd/ac/seu/my/hibernatespringdemo/Main.java
+++ NetBeansProjects/HibernateSpringDemo/src/main/java/bd/ac/seu/my/hibernatespringdemo/Main.java
@@ -7,27 +7,50 @@ package bd.ac.seu.my.hibernatespringdemo;
 
 import bd.ac.seu.my.hibernatespringdemo.molel.Student;
 import bd.ac.seu.my.hibernatespringdemo.utill.SessionFactorySingleton;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.hibernate.Session;
 
 /**
  *
  * @author shithilboss
  */
-public class Main {
+public class Main extends Application{
+        private static Stage mainStage;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/springDemoFXMLUI.fxml"));
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/springdemofxmlui.css");
+        
+        stage.setTitle("JavaFX and Maven");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static Stage getMainStage() {
+        return mainStage;
+    }
 
     public static void main(String[] args) {
-        Session session = SessionFactorySingleton.getSessionFactory().openSession();
-
-
-        session.beginTransaction();
-        Student student = new Student("2012200000026", "Shithil");
-        session.save(student);
-        session.getTransaction().commit();
-
-
-        session.close();
-        
-        SessionFactorySingleton.getSessionFactory().close();
+        launch(args);
+//      Session session = SessionFactorySingleton.getSessionFactory().openSession();
+//
+//
+//        session.beginTransaction();
+//       Student student = new Student();
+//        session.save(student);
+//        session.getTransaction().commit();
+//
+//
+//        session.close();
+//        
+//        SessionFactorySingleton.getSessionFactory().close();
 
     }
 
